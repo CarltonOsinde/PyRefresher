2addbada9f33e7a2a7d863b014063018c0fc9309
Carlton Osinde <47374849+CarltonOsinde@users.noreply.github.com>
2022-09-28 17:04:14 -0400

    Section 3-10 complete

--- UserInput.py
+++ UserInput.py
@@ -13,7 +13,28 @@ print(weather_condition(user_input))
 
 #String Formating
 user_input = input("Enter your name: ")
-message = "What it do %s" %user_input
+message = "What it do %s" %user_input # Variable
 message = f"What's good {user_input}" # Easier way to implement String formating
 
 print(message)
+
+
+#String Formating with multiple Variables
+
+name = input("Enter your first name: ")
+surname = input ("Enter your surname: ")
+
+message = "Yo %s %s" %(name, surname)
+
+print(message)
+
+
+def get_username():
+    user_input = input("Enter your first name: ")
+    first_name = user_input.capitalize()
+    message = "Hi %s" %first_name
+    
+    return message
+
+print(get_username())
+
