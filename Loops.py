616035154d30ae44a2b1b660ae737a05f23636b2
Carlton Osinde <47374849+CarltonOsinde@users.noreply.github.com>
2022-09-28 21:35:00 -0400

    Section 3 -10 complete

---
+++ Loops.py
@@ -0,0 +1,33 @@
+#LOOPS
+
+temps = [9.9,8.8,7.6]
+temps[0]
+
+
+for each_temperature in temps:
+     print(round(each_temperature))
+
+
+
+colors = [11, 34, 98, 43, 45, 54, 54]
+
+for i in colors:
+    if i > 50:
+        print(i)
+
+
+
+
+#Looping through a dictionary
+st_grades = {"Carlton": 91,"Dem people":90}
+
+for i in st_grades.items():
+    print(i)
+
+for i in st_grades.keys():
+    print(i)
+
+
+for i in st_grades.values():
+    print(i)
+
